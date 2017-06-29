24dff9b54546c74a604e26e23063e68928d09e44
josdem <joseluis.delacruz@gmail.com>
2017-06-29 11:22:53 -0500

    Adding person services

---
+++ src/main/groovy/com/jos/dem/jdbc/service/PersonService.groovy
@@ -0,0 +1,7 @@
+package com.jos.dem.service
+
+interface PersonService {
+	
+	List<Person> getPersons()
+
+}
---
+++ src/main/groovy/com/jos/dem/jdbc/service/impl/PersonServiceImpl.groovy
@@ -0,0 +1,11 @@
+package com.jos.dem.service.impl
+
+import com.jos.dem.service.PersonService
+
+interface PersonServiceImpl implements PersonService {
+	
+	List<Person> getPersons() {
+
+	}
+
+}
