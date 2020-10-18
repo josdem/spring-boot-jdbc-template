cc5ef5a472c4a4c0a86de75d2b26ce194a48a01d
josdem <joseluis.delacruz@gmail.com>
2020-10-18 13:03:49 -0400

    Converting properties to Yaml

--- src/main/java/com/jos/dem/springboot/jdbc/model/Person.java
+++ src/main/java/com/jos/dem/springboot/jdbc/model/Person.java
@@ -1,10 +1,12 @@
 package com.jos.dem.springboot.jdbc.model;
 
+import lombok.Data;
+
+@Data
 public class Person {
-	
+
 	private long id;
 	private String nickname;
 	private String email;
 	private int ranking;
-	
 }
